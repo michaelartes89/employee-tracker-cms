d275d4eb34edb5686e5f981696c8b28d5269f6b3
MDBA <artes.mike@gmail.com>
2020-01-30 20:26:04 -0500

    adding seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO departments (dept_name)
+VALUES ("SALES"), ("ENGINEERING"), ("LEGAL");
+
+INSERT INTO roles (title,salary, dept_id) 
+VALUES ("SALES REP", 90000.00, 1),
+("SALES MANAGER", 70000.00, 1),
+("LAWYER", 100000.00, 1),
+("ENGINEER", 90000.00, 1);
+
+INSERT INTO employee (first_name,last_name)
+VALUES ("Michael", "DAVID"), ("ARTES, BOWEN");
+
+
