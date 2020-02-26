1d4121d9c99be93ad0bc999d55b6157a36742149
MDBA <artes.mike@gmail.com>
2020-02-25 21:57:40 -0500

    adding updated seeds

--- seeds.sql
+++ seeds.sql
@@ -7,7 +7,7 @@ VALUES ("SALES REP", 90000.00, 1),
 ("LAWYER", 100000.00, 1),
 ("ENGINEER", 90000.00, 1);
 
-INSERT INTO employee (first_name,last_name)
-VALUES ("Michael", "DAVID"), ("ARTES, BOWEN");
+INSERT INTO employee (first_name,last_name,roles_id)
+VALUES ('Michael', 'DAVID',1), ('ARTES', 'BOWEN',2);
 
 
